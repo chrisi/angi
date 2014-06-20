bec61f9706830870d3de27f6711bfcf442a64f83
Christian Gebauer <mail@christiangebauer.de>
2014-06-20 19:00:36 +0200

    Liste und Editor kommunizieren nun miteinander

---
+++ contactApp.js
@@ -0,0 +1,44 @@
+angular.module('contactApp', ['forms'])
+    .factory('ContactStore', function ($http) {
+        var contacts = [];
+        return {
+            loadContacts: function () {
+                $http.get('contacts.json').then(function (articlesResponse) {
+                    contacts = articlesResponse.data;
+                });
+            },
+            updateContact: function (contact) {
+              var i = 0; // TODO: weiter hier
+            },
+            getContacts: function () {
+                return contacts;
+            }
+        }
+    })
+    .controller('ContactFormCtrl', function ($rootScope, $scope) {
+        $scope.submit = function (contact) {
+            $rootScope.$broadcast('contactSubmitted', contact);
+        };
+
+        $scope.reset = function () {
+            $scope.form = angular.copy($scope.pristine);
+        };
+
+        $scope.$on('contactSelected', function (event, args) {
+            $scope.form = args;
+            $scope.pristine = angular.copy(args);
+        });
+    })
+    .controller('ContactListCtrl', function ($rootScope, $scope, $http, ContactStore) {
+        $scope.store = ContactStore;
+        $scope.name = "Christian";
+
+        $scope.selectContact = function (contact) {
+            $rootScope.$broadcast('contactSelected', contact);
+        };
+
+        $scope.$on('contactSubmitted', function (event, args) {
+            ContactStore.updateContact(args);
+        });
+    })
+;
