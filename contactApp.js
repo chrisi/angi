e61a7d34be3825254eaab3bb34f035b6656e25ae
Christian Gebauer <mail@christiangebauer.de>
2014-06-30 09:27:10 +0200

    Ausbau Dialog-Directive -Classic Dialog tweaks -Angular-UI-Dialog integrated -Cleanup

--- contactApp.js
+++ contactApp.js
@@ -1,14 +1,14 @@
-angular.module('contactApp', ['forms'])
+angular.module('contactApp', ['forms', 'ui.bootstrap'])
     .factory('ContactStore', function ($http) {
         var contacts = [];
         return {
             loadContacts: function () {
-                $http.get('contacts.json').then(function (articlesResponse) {
+                $http.get('data/contacts.json').then(function (articlesResponse) {
                     contacts = articlesResponse.data;
                 });
             },
             updateContact: function (contact) {
-              var i = 0; // TODO: weiter hier
+                var i = 0; // TODO: weiter hier
             },
             getContacts: function () {
                 return contacts;
@@ -24,7 +24,7 @@ angular.module('contactApp', ['forms'])
             $scope.form = angular.copy($scope.pristine);
         };
 
-        $scope.$on('contactSelected', function (event, args) {
+        $scope.$on('contactSelected-rb', function (event, args) {
             $scope.form = args;
             $scope.pristine = angular.copy(args);
             $scope.showDialog = true;
@@ -34,12 +34,52 @@ angular.module('contactApp', ['forms'])
         $scope.store = ContactStore;
         $scope.name = "Christian";
 
-        $scope.selectContact = function (contact) {
-            $rootScope.$broadcast('contactSelected', contact);
+        $scope.selectContact = function (contact, mode) {
+            if (mode == 'rb')
+                $rootScope.$broadcast('contactSelected-rb', contact);
+            if (mode == 'ng')
+                $rootScope.$broadcast('contactSelected-ng', contact);
         };
 
         $scope.$on('contactSubmitted', function (event, args) {
             ContactStore.updateContact(args);
         });
     })
+    .controller('ModalInstanceCtrl', function ($scope, $modalInstance, form) {
+        $scope.form = form;
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.form);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    })
+    .controller('ModalDemoCtrl', ['$scope', '$modal', '$log', function ($scope, $modal, $log) {
+        $scope.open = function (origform) {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                size: 'lg',
+                resolve: {
+                    form: function () {
+                        return angular.copy(origform);
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (editForm) {
+                angular.copy(editForm, origform);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.$on('contactSelected-ng', function (event, args) {
+            $scope.open(args);
+        });
+
+    }])
 ;
